127334f33bddb15e4785a7da60a4033adce9eff1
sonjaya <sonjaya@sonjaya-PC>
2015-10-20 23:15:00 +0700

    First Commit - TL

---
+++ application/models/mahasiswaModel.php
@@ -0,0 +1,19 @@
+<?php
+
+
+ class mahasiswaModel extends CI_Model {
+
+     function __construct() {
+         parent::__construct();
+    }
+    
+    function getAll(){
+        
+    }
+
+}
+
+?>
+
+
+
---
+++ application/views/welcome_message.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/css/style.css">
+</head>
+<body>
+    
+    <nav class="navbar navbar-default" style="background-color: #AD0000">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="#">
+                    <span class="glyphicon glyphicon-home"></span>
+                </a>
+            </div>
+        </div>
+    </nav>
+  
+    <center>
+        <div style="margin-top: 250px"></div>
+          <div class="alert alert-danger" role="alert">
+        <h1>
+            <span class="glyphicon glyphicon-wrench" ></span>
+            RHESUS app 
+        </h1>
+        <h1>
+            under Contruction !
+        </h1>
+          </div>
+    </center>
+        
+
+
+    
+
+    
+<script type="text/javascript" src="<?php echo base_url();?>assets/js/jquery-1.9.1.min"></script>
+<script type="text/javascript" src="<?php echo base_url();?>bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
